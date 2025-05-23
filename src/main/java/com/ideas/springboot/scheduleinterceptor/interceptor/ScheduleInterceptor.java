2703e92af81674ef16899ad9cee77ac745a57fc7
heimedina <heidimedina23@gmail.com>
2025-05-23 12:49:45 +0200

    update Interceptor

--- src/main/java/com/ideas/springboot/scheduleinterceptor/interceptor/ScheduleInterceptor.java
+++ src/main/java/com/ideas/springboot/scheduleinterceptor/interceptor/ScheduleInterceptor.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -42,7 +43,10 @@ public class ScheduleInterceptor implements HandlerInterceptor{
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 		String mensaje = (String) request.getAttribute("mensaje");
-		modelAndView.addObject("horario",mensaje);
+		if(modelAndView != null && handler instanceof HandlerMethod) {
+			modelAndView.addObject("horario",mensaje);
+		}
+		
 	}
 
 }
